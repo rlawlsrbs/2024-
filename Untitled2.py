c08600097d36ee82040fe2c193746e7fb5d8edd4
rlawlsrbs <162547586+rlawlsrbs@users.noreply.github.com>
2024-03-07 16:15:02 +0900

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello, world")
+
+
+# In[2]:
+
+
+a = 1
+b = 2
+c = a + b
+print(c)
+
+
+# In[3]:
+
+
+import math
+
+int(3.5)
+
+
+# In[4]:
+
+
+# 20240307
+# 파이썬 기본 데이터 타입
+
+
+# In[6]:
+
+
+korea = "동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세"
+print(korea)
+
+print(korea)
+
+print(korea)
+
+
+# In[9]:
+
+
+#산술연산자
+a = 5
+b = 2
+
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+
+print(c)
+print(d)
+print(e)
+print(f)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[21]:
+
+
+#비교연산자
+a = 1
+if a <= 2:
+    
+print("a는 2보다 작거나 같다")
+
+
+# In[ ]:
+
+
+
+
+
+# In[20]:
+
+
+a = a / 10
+a /= 10
+print(a)
+
+
+# In[31]:
+
+
+x = True
+y = False
+
+if x and y:
+    print("Yes")
+else:
+    print("No")
+    
+if x or y:
+    print("Yes")
+else:
+    print("No")
+    
+if x and  not y:
+    print("Yes")
+else:
+    print("No")
+    
+
+
+# In[29]:
+
+
+a = [1,2,3,4]
+b = 3 in a    #True
+print(b)
+
+
+# In[32]:
+
+
+a = "ABC"
+b = a
+print(a is b)   #True
+
+
+# In[ ]:
+
+
+
+
