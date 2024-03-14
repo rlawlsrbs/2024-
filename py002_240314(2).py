87d871bb239758aec09603417d996258741cc207
rlawlsrbs <162547586+rlawlsrbs@users.noreply.github.com>
2024-03-14 17:45:09 +0900

    Add files via upload

---
+++ py002_240314(2).py
@@ -0,0 +1,38 @@
+#"""
+#print("hello world")
+#print("hello  world")
+#print("hello"+"world")
+#print("hello" + " " + "world")
+#"""
+
+#"""
+#print("오늘은 파이썬 챌린지 3일차 입니다)
+#print('디버깅 "+" 연습중입니다')
+ #print('내일은 ("금요일" + "입니다)')
+ #print(("그래서 챌린지가 없습니다"))
+ #"""
+
+#"""print ("나는" + input("당신의 이름은 무엇입니까?") + "입니다")"""
+
+#"""
+#name = input("당신의 이름은 무엇입니까?")
+#print(김진균)
+#"""
+
+#"""
+#name = input("당신의 이름은 무엇입니까?")
+#length = len(name)
+#print(length)
+#"""
+
+#a = input("a:")
+#b = input("b:")
+
+#c = a
+#a = b
+#b = c
+
+#print("a = " + a)
+#print("b = " + b)
+ 
+print("내가 좋아하는 영화는" + input("바람") + "입니다" + "주인공은" + input("정우") + "입니다")
