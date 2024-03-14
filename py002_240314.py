83b46dfc0c28fd9d37aabc8eb4e5f5a881c46f80
rlawlsrbs <162547586+rlawlsrbs@users.noreply.github.com>
2024-03-14 17:41:09 +0900

    Add files via upload

---
+++ py002_240314.py
@@ -0,0 +1,87 @@
+#문자열
+
+#p = "이름: %s 나이 : %d" % ("김유신", 65)
+#print(p)
+
+#p = "X = %0.3f, Y = %10.2f" % (3.141592, 3.141592)
+#print(p)
+
+#s = "ABC"
+#type(s)
+#v = s[1]
+#type(s[1])
+
+#path = r'c:\Temp\test.csv'
+#print(path)
+
+#s = ','.join(['가나', '다라', '마바'])
+#print(s)
+
+#s = ''.join(['가나', '다라', '마바'])
+#print(s)
+
+#if a == 10:
+  #print('10입니다')
+
+#if a == 10:
+    #print('10입니다')
+
+#if a == 10:
+    #print('10입니다')
+
+#X = 9
+#if x < 10:
+    #print(x)
+    #print("한자리수")
+
+#if x < 100: print(X)
+
+#X = 100
+#if 2 < 10:
+    #print("한자리수")
+#elif x < 100:
+    #print("두자리수")
+#else:
+    #print("세자리 이상")
+
+#n = 20
+#if n < 10 :
+    #pass
+#else:
+    #print(n)
+
+
+#x=input('123')
+#print(x)
+
+#i=1
+#while i <= 10:
+    #print(i)
+    #i +=1
+
+#sum = 0
+#for i in range(11):
+    #sum += i
+#print(sum)
+
+#list = ["This", "is", "a", "book"]
+#for s in list:
+    #print(s)
+
+#i = 0 
+#sum = 0
+#while True:
+    #i += 1
+    #if i == 5:
+        #continue
+    #if i > 10:
+        #break
+    #sum += i
+    
+#print(sum)
+
+#numbers = range (2, 11, 2)
+
+#for x in numbers:
+    #print(x)
+
